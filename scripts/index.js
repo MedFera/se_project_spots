e7a289444054c3bdbc28d70630a3389de4bf6c83
Medin Feratovic <medferatovic@gmail.com>
2024-09-18 22:31:15 -0400

    Added Profile Editing and Dynamic Card Elements

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const initialCards = [
   {name: "Mountain house", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"}
 ];
 
+//Modal Functionality
 const profileEditButton = document.querySelector(".profile__edit-btn");
 
 const editModal = document.querySelector("#edit-modal");
@@ -14,14 +15,66 @@ const editModalCloseButton = editModal.querySelector(".modal__close-btn");
 
 function openModal() {
   editModal.classList.add("modal_opened");
-  console.log("visible");
-};
+  getModalInputs();
+  //console.log("visible");
+}
 
 function closeModal() {
   editModal.classList.remove("modal_opened");
-  console.log("hidden");
+  //console.log("hidden");
 }
 
-profileEditButton.addEventListener("click", openModal);
+//Edit and Update Profile
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__description");
+const nameInput = document.querySelector("#profile-name-input");
+const jobInput = document.querySelector("#profile-description-input");
 
+function getModalInputs() {
+
+nameInput.value = profileNameElement.textContent;
+jobInput.value = profileJobElement.textContent;
+}
+
+const profileFormElement = document.querySelector(".modal__form");
+
+function handleProfileFormSubmit(evt){
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+  editModal.classList.remove("modal_opened");
+}
+
+
+//Event Listeners
+profileEditButton.addEventListener("click", openModal);
 editModalCloseButton.addEventListener("click", closeModal);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit,true);
+
+
+
+//Build cards
+const cardsList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template");
+
+function getCardElement(data){
+  let cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+  cardElement .querySelector(".card__image").setAttribute("src", data.link);
+  cardElement .querySelector(".card__image").setAttribute("alt", data.name);
+  cardElement .querySelector(".card__title").textContent = data.name;
+  return cardElement
+}
+
+function generateCards(){
+
+for(let i = 0; i <= (initialCards.length-1); i++){
+
+  //console.log(newCard);
+  let newCard = getCardElement(initialCards[i]);
+  cardsList.appendChild(newCard);
+
+}
+
+}
+
+generateCards()
